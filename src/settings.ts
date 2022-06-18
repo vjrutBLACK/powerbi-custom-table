bb74ecb3470299dc84464145b1581ba141e90aa0
natu97 <natu991997@gmail.com>
2022-06-18 15:45:50 +0700

    first commit

---
+++ src/settings.ts
@@ -0,0 +1,17 @@
+"use strict";
+
+import { dataViewObjectsParser } from "powerbi-visuals-utils-dataviewutils";
+import DataViewObjectsParser = dataViewObjectsParser.DataViewObjectsParser;
+
+export class VisualSettings extends DataViewObjectsParser {
+    public dataPoint: dataPointSettings = new dataPointSettings();
+    public config: configSettings = new configSettings();
+}
+
+export class dataPointSettings {
+    public tableConfiguration: string = "";
+}
+
+export class configSettings {
+    public exportExcel: boolean = false;
+}
